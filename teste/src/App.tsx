4bfb6ede72699e2cb3aa12833ff8e86d7a9f0648
Artur Oliveira Gomes <artur.o.gomes@gmail.com>
2022-09-13 19:20:07 -0400

    feat: :sparkles: Adicionando props no Card

--- teste/src/App.tsx
+++ teste/src/App.tsx
@@ -2,11 +2,22 @@ import { useState } from 'react'
 import './App.css'
 import Card from './Components/Card'
 // yarn add axios
+const usuario = {
+  name: "Artur",
+  email: "artur.gomes@ufms.br",
+  address: {
+    street: "Rua Rio Branco, ...",
+    suite: "CPAN",
+    city: "Corumbá"
+
+  },
+  phone: "12345"
+}
 function App() {
 
   return (
     <>
-      <Card />
+      <Card user={usuario} />
     </>
   )
 }
--- teste/src/Components/Card/index.tsx
+++ teste/src/Components/Card/index.tsx
@@ -2,26 +2,29 @@ import React from 'react';
 import { MdLocationOn, MdPhone } from 'react-icons/md'
 import './styles.css'
 
-const Card: React.FC = () => {
+interface CardInterface {
+  user: any;
+}
+const Card: React.FC = ({ user }: CardInterface) => {
   return (
     <>
       <div className="card">
         <div className="card_left">
-          <div className="card_title">Leanne Graham</div>
-          <div className="card_subtitle">incere@april.biz</div>
+          <div className="card_title">{user.name}</div>
+          <div className="card_subtitle">{user.email}</div>
         </div>
         <div className="card_right">
           <div className="address_container">
             <MdLocationOn size={30} />
             <div className="address">
-              <span>Kulas Light</span>
-              <span>Apt. 556</span>
-              <span>Gwenborough</span>
+              <span>{user.address.street}</span>
+              <span>{user.address.suite}</span>
+              <span>{user.address.city}</span>
             </div>
           </div>
           <div className="address_container">
             <MdPhone size={30} />
-            <div className="address">1-770-736-8031 x56442</div>
+            <div className="address">{user.phone}</div>
           </div>
         </div>
       </div>
