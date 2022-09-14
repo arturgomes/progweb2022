61f04c2cab6e0842503dc8cb64096f8f3e8aabdc
Artur Oliveira Gomes <artur.o.gomes@gmail.com>
2022-09-13 20:08:07 -0400

    feat(CardList): :sparkles: Search Bar

--- teste/src/Components/CardList/index.tsx
+++ teste/src/Components/CardList/index.tsx
@@ -7,7 +7,7 @@ import './styles.css';
 const CardList: React.FC = () => {
   const [users, setUsers] = useState([])
   const [loading, setLoading] = useState(true);
-
+  const [filter, setFilter] = useState('')
   useEffect(() => {
     let consultaAPI = async () => {
       await api.get('/users')
@@ -16,14 +16,38 @@ const CardList: React.FC = () => {
           setLoading(false)
         })
     }
+
     if (loading) {
       consultaAPI();
     }
   }, [])
+  function handleSearchText(event: any) {
+    console.log(event.target.value)
+    setFilter(event.target.value)
+  }
+  function displayContact() {
+    if (filter) {
+      return users
+        .filter((user: any) =>
+          user.name.toLowerCase()
+            .includes(filter.toLowerCase()) ||
+          user.address.city.toLowerCase()
+            .includes(filter.toLowerCase())
+        ).map(usuario => <Card user={usuario} />)
+    }
+    else {
+      return users.map(usuario => <Card user={usuario} />)
+    }
+  }
 
-  return (<div className="card_list_container">
-    {users.map(usuario => <Card user={usuario} />)}
-  </div>);
+  if (loading) {
+    return <div>Carregando..</div>
+  } else {
+    return (<div className="card_list_container">
+      <input type="text" value={filter} onChange={handleSearchText} />
+      {displayContact()}
+    </div>);
+  }
 }
 
 export default CardList;
