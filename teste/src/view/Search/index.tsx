76ad31b23cffcafeac8d009feb003a26cef4a9f3
Artur Oliveira Gomes <artur.o.gomes@gmail.com>
2022-09-27 18:54:50 -0400

    feat: :sparkles: adicionando CardSearch

--- teste/src/view/Search/index.tsx
+++ teste/src/view/Search/index.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import CardSearch from '../../Components/CardSearch';
+import Header from '../../Components/Header';
 // import { Container } from './styles';
 
 const Search: React.FC = () => {
   return (<div className="app_wrapper">
-    <header>
-      <ul>
-        <li><Link to="/">Listar</Link></li>
-        <li><Link to="/buscar">Buscar</Link></li>
-        <li><Link to="/sobre">Sobre</Link></li>
-      </ul>
-    </header>
-    <h1>Buscando contatos</h1>
+    <Header title="Buscando contatos" />
+    <CardSearch />
   </div>);
 
 }
